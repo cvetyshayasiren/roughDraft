8b2282b923076ca8083164be412e08e6c0dc39af
cvetyshayasiren <quadthirteen4@gmail.com>
2025-07-27 19:06:26 +0300

    hello roughDraft

---
+++ composeApp/src/commonMain/kotlin/cvetyshayasiren/roughdraft/domain/map/TileLink.kt
@@ -0,0 +1,27 @@
+package cvetyshayasiren.roughdraft.domain.map
+
+
+sealed interface TileLink {
+    val label: String
+    fun getLink(z: Int, x: Int, y: Int): String
+
+    class StandartOSM(): TileLink {
+        override val label: String = "Open Street Map"
+        override fun getLink(z: Int, x: Int, y: Int): String =
+            "https://tile.openstreetmap.org/$z/$x/$y.png"
+    }
+
+    class CyclOSM(): TileLink {
+        override val label: String = "Велодорожки"
+
+        override fun getLink(z: Int, x: Int, y: Int): String =
+            "https://b.tile-cyclosm.openstreetmap.fr/cyclosm/$z/$x/$y.png"
+    }
+
+    class WaterColors(): TileLink {
+        override val label: String = "Акварель"
+
+        override fun getLink(z: Int, x: Int, y: Int): String =
+            "https://watercolormaps.collection.cooperhewitt.org/tile/watercolor/$z/$x/$y.jpg"
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/cvetyshayasiren/roughdraft/ui/test/PlayerTest.kt
@@ -0,0 +1,35 @@
+package cvetyshayasiren.roughdraft.ui.test
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialShapes
+import androidx.compose.material3.toShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import cvetyshayasiren.roughdraft.ui.features.audioPlayer.AudioPlayerView
+import roughdraft.composeapp.generated.resources.Res
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun PlayerTest() {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AudioPlayerView(
+            modifier = Modifier
+                .width(600.dp)
+                .wrapContentHeight()
+                .clip(MaterialShapes.Slanted.toShape())
+                .background(Color(105, 105, 210))
+                .padding(48.dp),
+            soundUri = Res.getUri("files/train.mp3")
+        )
+    }
+}
